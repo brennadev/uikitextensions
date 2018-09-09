75fd699732975c80f5682539135c69c1aa44dac1
Brenna Olson <36449761+brennadev@users.noreply.github.com>
2018-09-09 13:22:35 -0500

    Xcode project created

---
+++ UIKitExtensions/UIKitExtensions/UIKitExtensions.h
@@ -0,0 +1,19 @@
+//
+//  UIKitExtensions.h
+//  UIKitExtensions
+//
+//  Created by Brenna Olson on 9/9/18.
+//  Copyright © 2018 Brenna Olson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIKitExtensions.
+FOUNDATION_EXPORT double UIKitExtensionsVersionNumber;
+
+//! Project version string for UIKitExtensions.
+FOUNDATION_EXPORT const unsigned char UIKitExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKitExtensions/PublicHeader.h>
+
+
